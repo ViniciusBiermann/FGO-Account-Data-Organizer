9bbd87a5c90ca91c1ed365ee25c75c4177a41b42
Vinicius <viniciusbiermann@hotmail.com>
2023-02-11 10:18:15 -0300

    Added custom rarity filter to servants datatable.

--- static/fgo/js/script.js
+++ static/fgo/js/script.js
@@ -5,12 +5,58 @@ $(document).ready( function () {
     order: [[6, 'asc']],
     pagingType: 'full_numbers',
     paging: false,
+    responsive: true
     });
 
-    $('div.toolbar').html($('#servantClassFilterComponent'));
-    $('div.toolbar').html($('#rarityFilterComponent'));
+    var rarityIndex = 0;
+      $("#servants_table th").each(function (i) {
+        if ($($(this)).html() == "Rarity") {
+          rarityIndex = i; return false;
+        }
+      });
+
+    $.fn.dataTable.ext.search.push(
+        function (settings, data, dataIndex) {
+          var rarities_toggled = getToggledServantRarities()
+          var rarityColumn = data[rarityIndex];
+          if (rarities_toggled.includes(rarityColumn)) {
+            return true;
+          }
+          return false;
+        }
+      );
 } );
 
+function getToggledServantRarities() {
+    var values = []
+    for (var i = 1; i < 6; i++) {
+       var button = $('#rarityButton' + i)
+       if (button.hasClass('active')) {
+            values.push(button.val())
+       }
+    }
+    return values
+}
+
+var buttonClick = function()
+{
+    if (this.classList.contains('active')) {
+        this.style.color = 'yellow'
+    } else {
+        this.style.color = 'grey'
+    }
+    $('#servants_table').DataTable().draw();
+}
+
+function redrawTable() {
+    $('#servants_table').DataTable().draw();
+};
+
+document.getElementById('rarityButton1').onclick = buttonClick;
+document.getElementById('rarityButton2').onclick = buttonClick;
+document.getElementById('rarityButton3').onclick = buttonClick;
+document.getElementById('rarityButton4').onclick = buttonClick;
+document.getElementById('rarityButton5').onclick = buttonClick;
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
